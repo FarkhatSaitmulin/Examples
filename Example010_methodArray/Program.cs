549b727b122c1268cd054f87a35da6b39b3eab98
Frenk KAZ <farhats81@mail.ru>
2022-11-23 00:22:08 +0600

    Находим индекс из массива

---
+++ Example010_methodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {11, 12, 13, 14, 15, 16, 17, 18, 19, 13};
+
+int n = array.Length;
+int find = 13;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array [index] == find)
+    {
+    Console.WriteLine(index);
+    }
+    index = index + 1;
+}
